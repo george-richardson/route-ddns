9ef059abd5de4efbf50ddbdc3e2959e406a5e51c
George Richardson <george@gjhr.me>
2019-01-29 21:10:59 +0000

    Reduce nesting when resolving IP

--- resolveIP.go
+++ resolveIP.go
@@ -3,11 +3,9 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net"
 	"net/http"
 	"strings"
-	"time"
 
 	try "gopkg.in/matryer/try.v1"
 )
@@ -16,29 +14,30 @@ func resolveIP(provider string) (string, error) {
 	const maxAttempts = 5
 	var resolvedIP string
 	err := try.Do(func(attempt int) (bool, error) {
-		var resp, err = http.Get(provider)
-		if err == nil {
-			defer resp.Body.Close()
-			var bodyBytes []byte
-			bodyBytes, err = ioutil.ReadAll(resp.Body)
-			if err == nil {
-				if resp.StatusCode != http.StatusOK {
-					err = fmt.Errorf("Received non 200 status code '%v' from '%v'", resp.StatusCode, provider)
-				} else {
-					resolvedIP = strings.TrimSpace(string(bodyBytes))
-					if net.ParseIP(resolvedIP) == nil {
-						err = fmt.Errorf("Response invalid, unable to parse IP")
-					}
-				}
-			}
-		}
-		if err != nil {
-			log.Print(fmt.Sprintf("WARNING: Resolve attempt %v/%v: %v", attempt, maxAttempts, err))
-			if attempt != maxAttempts {
-				time.Sleep(time.Duration(attempt*attempt) * time.Second)
-			}
-		}
+		var err error
+		resolvedIP, err = tryResolveIP(provider)
 		return attempt < maxAttempts, err
 	})
+
 	return resolvedIP, err
+}
+
+func tryResolveIP(provider string) (string, error) {
+	resp, err := http.Get(provider)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("Received non 200 status code '%v' from '%v'", resp.StatusCode, provider)
+	}
+	resolvedIP := strings.TrimSpace(string(bodyBytes))
+	if net.ParseIP(resolvedIP) == nil {
+		return "", fmt.Errorf("Response invalid, unable to parse IP")
+	}
+	return resolvedIP, nil
 }
